eb03aeeb5be523c516271e70f67101b38bc648e6
Linas <jusys.linas@gmail.com>
2018-04-12 11:14:47 +0300

    Update superpwa-multisite.php
    Added changes from v1.5 to overridden functions.

--- superpwa-multisite.php
+++ superpwa-multisite.php
@@ -118,108 +118,109 @@ if ( file_exists( $pwa_plugin_dir ) && is_plugin_active( $pwa_plugin_name ) ) {
 		ob_start();
 	if ( false ): ?>
         <script type="text/javascript"><?php endif; ?>
-          'use strict';
+            'use strict';
 
-          /**
-           * Service Worker of SuperPWA
-           * https://wordpress.org/plugins/super-progressive-web-apps/
-           */
+            /**
+             * Service Worker of SuperPWA
+             * https://wordpress.org/plugins/super-progressive-web-apps/
+             */
 
-          const cacheName = '<?php echo parse_url( get_bloginfo( 'wpurl' ), PHP_URL_HOST ) . '-superpwa-' . SUPERPWA_VERSION; ?>';
-          const startPage = '<?php echo superpwa_get_start_url(); ?>';
-          const offlinePage = '<?php echo get_permalink( $settings['offline_page'] ) ? trailingslashit( get_permalink( $settings['offline_page'] ) ) : trailingslashit( get_bloginfo( 'wpurl' ) ); ?>';
-          const fallbackImage = '<?php echo $settings['icon']; ?>';
-          const filesToCache = [startPage, offlinePage, fallbackImage];
-          const neverCacheUrls = [/\/wp-admin/, /\/wp-login/, /preview=true/];
+            const cacheName = '<?php echo parse_url( get_bloginfo( 'wpurl' ), PHP_URL_HOST ) . '-superpwa-' . SUPERPWA_VERSION; ?>';
+            const startPage = '<?php echo superpwa_get_start_url(); ?>';
+            const offlinePage = '<?php echo get_permalink($settings['offline_page']) ? trailingslashit(get_permalink($settings['offline_page'])) : trailingslashit(get_bloginfo( 'wpurl' )); ?>';
+            const fallbackImage = '<?php echo $settings['icon']; ?>';
+            const filesToCache = [startPage, offlinePage, fallbackImage];
+            const neverCacheUrls = [/\/wp-admin/,/\/wp-login/,/preview=true/];
 
-          // Install
-          self.addEventListener('install', function(e) {
-            console.log('SuperPWA service worker installation');
-            e.waitUntil(
-                caches.open(cacheName).then(function(cache) {
-                  console.log('SuperPWA service worker caching dependencies');
-                  return cache.addAll(filesToCache);
-                }),
-            );
-          });
+            // Install
+            self.addEventListener('install', function(e) {
+                console.log('SuperPWA service worker installation');
+                e.waitUntil(
+                    caches.open(cacheName).then(function(cache) {
+                        console.log('SuperPWA service worker caching dependencies');
+                        return cache.addAll(filesToCache);
+                    })
+                );
+            });
 
-          // Activate
-          self.addEventListener('activate', function(e) {
-            console.log('SuperPWA service worker activation');
-            e.waitUntil(
-                caches.keys().then(function(keyList) {
-                  return Promise.all(keyList.map(function(key) {
-                    if (key !== cacheName) {
-                      console.log('SuperPWA old cache removed', key);
-                      return caches.delete(key);
-                    }
-                  }));
-                }),
-            );
-            return self.clients.claim();
-          });
+            // Activate
+            self.addEventListener('activate', function(e) {
+                console.log('SuperPWA service worker activation');
+                e.waitUntil(
+                    caches.keys().then(function(keyList) {
+                        return Promise.all(keyList.map(function(key) {
+                            if ( key !== cacheName ) {
+                                console.log('SuperPWA old cache removed', key);
+                                return caches.delete(key);
+                            }
+                        }));
+                    })
+                );
+                return self.clients.claim();
+            });
 
-          // Fetch
-          self.addEventListener('fetch', function(e) {
+            // Fetch
+            self.addEventListener('fetch', function(e) {
 
-            // Return if the current request url is in the never cache list
-            if (!neverCacheUrls.every(checkNeverCacheList, e.request.url)) {
-              console.log('SuperPWA: Current page is excluded from cache');
-              return;
+                // Return if the current request url is in the never cache list
+                if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
+                    console.log('SuperPWA: Current page is excluded from cache');
+                    return;
+                }
+
+                // Return if request url protocal isn't http or https
+                if ( ! e.request.url.match(/^(http|https):\/\//i) )
+                    return;
+
+                // Return if request url is from an external domain.
+                if ( new URL(e.request.url).origin !== location.origin )
+                    return;
+
+                // For POST requests, do not use the cache. Serve offline page if offline.
+                if ( e.request.method !== 'GET' ) {
+                    e.respondWith(
+                        fetch(e.request).catch( function() {
+                            return caches.match(offlinePage);
+                        })
+                    );
+                    return;
+                }
+
+                // Revving strategy
+                if ( e.request.mode === 'navigate' && navigator.onLine ) {
+                    e.respondWith(
+                        fetch(e.request).then(function(response) {
+                            return caches.open(cacheName).then(function(cache) {
+                                cache.put(e.request, response.clone());
+                                return response;
+                            });
+                        })
+                    );
+                    return;
+                }
+
+                e.respondWith(
+                    caches.match(e.request).then(function(response) {
+                        return response || fetch(e.request).then(function(response) {
+                            return caches.open(cacheName).then(function(cache) {
+                                cache.put(e.request, response.clone());
+                                return response;
+                            });
+                        });
+                    }).catch(function() {
+                        return caches.match(offlinePage);
+                    })
+                );
+            });
+
+            // Check if current url is in the neverCacheUrls list
+            function checkNeverCacheList(url) {
+                if ( this.match(url) ) {
+                    return false;
+                }
+                return true;
             }
 
-            // Return if request url protocal isn't http or https
-            if (!e.request.url.match(/^(http|https):\/\//i))
-              return;
-
-            // Return if request url is from an external domain.
-            if (( new URL(e.request.url).origin !== location.origin ) && ( e.request.mode === 'no-cors' ))
-              return;
-
-            // For POST requests, do not use the cache. Serve offline page if offline.
-            if (e.request.method !== 'GET') {
-              e.respondWith(
-                  fetch(e.request).catch(function() {
-                    return caches.match(offlinePage);
-                  }),
-              );
-              return;
-            }
-
-            // Revving strategy
-            if (e.request.mode === 'navigate' && navigator.onLine) {
-              e.respondWith(
-                  fetch(e.request).then(function(response) {
-                    return caches.open(cacheName).then(function(cache) {
-                      cache.put(e.request, response.clone());
-                      return response;
-                    });
-                  }),
-              );
-              return;
-            }
-
-            e.respondWith(
-                caches.match(e.request).then(function(response) {
-                  return response || fetch(e.request).then(function(response) {
-                    return caches.open(cacheName).then(function(cache) {
-                      cache.put(e.request, response.clone());
-                      return response;
-                    });
-                  });
-                }).catch(function() {
-                  return caches.match(offlinePage);
-                }),
-            );
-          });
-
-          // Check if current url is in the neverCacheUrls list
-          function checkNeverCacheList(url) {
-            if (this.match(url)) {
-              return false;
-            }
-            return true;
-          }
 			<?php if ( false ): ?></script><?php endif;
 
 		return ob_get_clean();
